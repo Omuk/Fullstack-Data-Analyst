cd77a1c436d92954f40059961532121518278faa
Victory Amunga <83257399+Omuk@users.noreply.github.com>
2022-08-08 17:42:18 -0700

    prog

--- Ushindi Website/app_home/urls.py
+++ Ushindi Website/app_home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('orphanhome', views.orphanhome, name='orphanhome'),
+    path('highschool', views.highschool, name='highschool'),
+    path('intermountain', views.intermountain, name='intermountain'),
+    path('song', views.song, name='song'),
+    path('projects', views.projects, name='projects'),
 ]
--- Ushindi Website/app_home/views.py
+++ Ushindi Website/app_home/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app_home/index.html')
+
+def orphanhome(request):
+    return render(request, 'app_home/orphanhome.html')
+
+def highschool(request):
+    return render(request, 'app_home/highschool.html')
+
+def intermountain(request):
+    return render(request, 'app_home/intermountain.html')
+
+def ics(request):
+    return render(request, 'app_home/ics.html')
+
+def song(request):
+    return render(request, 'app_home/song.html')
+
+def projects(request):
+    return render(request, 'app_home/projects.html')
+
+
+
