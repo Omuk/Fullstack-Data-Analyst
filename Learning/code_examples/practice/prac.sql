5a128455c83ba17a29cc13008b6193695b269eac
Victory Amunga <83257399+Omuk@users.noreply.github.com>
2022-10-04 15:58:47 -0700

    sql

---
+++ Learning/code_examples/practice/prac.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE Mydatabase;
+
+CREATE TABLE MyTable(
+    PersonID int,
+    Names VARCHAR(100),
+    City VARCHAR(100)
+    ToDrop VARCHAR(100)
+);
+
+CREATE INDEX MyTableIndex
+ON MyTable(PersonID, Names, City);
+
+INSERT INTO MyTable
+VALUES (
+    '6568', 'Jesse, James', 'San Diego'
+    '8574', 'Michael, Echo', 'Los Anges',
+    '8573', 'Poko,Paka', 'San Francisco',
+);
+
+ALTER TABLE MyTable
+DROP COLUMN VARCHAR;
+
+ALTER TABLE MyTable
+ADD State VARCHAR(100);
+
+Select PersonID
+FROM Mytable 
+WHERE PersonID=6568
+GROUP BY 
+HAVING
+ORDER BY
+
+SELECT MyTable.PersonID, MyTable.Names
+FROM MyTable LEFT JOIN MyTable2 ON MyTable.PersonID = MyTable2.PersonID
+
+
